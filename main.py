ba328e33f6126010543547ae9f4a42f542d7ddb2
chojungho1 <chojungho@goedu.kr>
2026-06-16 07:44:21 +0000

    최종(제출)

--- main.py
+++ main.py
@@ -59,24 +59,3 @@ print("\n==================================")
 print(" 가상 맞춤형 헬스케어 식단 프로그램 ")
 print("==================================")
 
-user_weight = float(input("가상 사용자의 체중(kg)을 입력하세요: "))
-user_activity = input("가상 사용자의 활동량을 선택하세요 (1:낮음 / 2:보통 / 3:높음): ")
-
-my_limit_calories = calculate_recommended_calories(user_weight, user_activity)
-print(f"\n[분석 결과] 가상 사용자의 하루 권장 칼로리는 {my_limit_calories}kcal 입니다.")
-
-meal_limit = my_limit_calories / 3
-print(f"[안내] 한 끼 권장 제한 칼로리는 약 {meal_limit:.1f}kcal 입니다.")
-
-print("\n1: 다이어트 (고단백 추천)")
-print("2: 벌크업 (고탄수화물 추천)")
-user_choice = input("원하는 식단 목적의 번호를 입력하세요 (1 또는 2): ")
-
-recommend_recipe = filter_menu(food_data, user_choice, meal_limit)
-
-if not recommend_recipe:
-    print("\n[알림] 조건(한 끼 제한 칼로리 및 영양소 규칙)에 맞는 음식을 찾지 못했습니다.")
-else:
-    print(f"\n★ 한 끼 {meal_limit:.1f}kcal 이하 맞춤형 가상 추천 식단 ★")
-    for item in recommend_recipe:
-        print(f"- {item[0]} (칼로리:{item[1]}kcal | 탄수화물:{item[2]}g, 단백질:{item[3]}g)")
